75cbf4965591e7950432325b283a02b611eff64c
Phillip Jackson <unisys12@gmail.com>
2020-11-09 13:01:52 -0600

    add markdown attrs support and detect env

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
-module.exports = (config)=>{
-    config.addPassthroughCopy('src/css');
-    config.addPassthroughCopy('src/imgs');
-    return {
-        dir: {
-            input: "src",
-            output: "_site"
-        },
-        passthroughFileCopy: true
-    };
-}
+module.exports = (config) => {
+  config.addPassthroughCopy("src/css");
+  config.addPassthroughCopy("src/imgs");
+
+  // Setup Markdown-it to parse Markdown files
+  let markdownIt = require("markdown-it");
+  var markdownItAttrs = require("markdown-it-attrs");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  config.setLibrary("md", markdownLib);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+    passthroughFileCopy: true,
+  };
+};
