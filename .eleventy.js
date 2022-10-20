39a20cfca146c46d43cae474aab359aef7416d9d
Phillip Jackson <unisys12@gmail.com>
2022-10-20 09:02:54 -0500

    add sitemap to site

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy("src/css/style.css");
   config.addPassthroughCopy("src/imgs");
   config.addPassthroughCopy("src/js");
+  config.addPassthroughCopy("src/robots.txt");
   return {
     dir: {
       input: "src",
